287f0f6d46abd77b74bc9350e6f9a9f9a7370aaf
Donatas <58343625+DonatasRa@users.noreply.github.com>
2021-12-10 20:59:24 +0200

    Init Commit

---
+++ MVCRazor/ZooWebAppSolution/ZooWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZooWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
