5993501b0ae3123cd3e6852d4646e120fb488e8b
Donatas <58343625+DonatasRa@users.noreply.github.com>
2021-12-27 18:40:12 +0200

    One to many

---
+++ EFCore/ShopListApp/ShopListApp/Dtos/AddShopItem.cs
@@ -0,0 +1,15 @@
+﻿using ShopListApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopListApp.Dtos
+{
+    public class AddShopItem
+    {
+        public ShopItem ShopItems { get; set; }
+        public List<Shop> AllShops { get; set; }
+    }
+}
